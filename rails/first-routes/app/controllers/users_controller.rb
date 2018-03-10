dccdfe03b6af5640e814e575e0e70bf2b356d796
Jeff Brubaker <jtbrubak1@gmail.com>
2018-03-09 23:43:37 -0500

    Add first routes

---
+++ rails/first-routes/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+    render plain: "I'm in the index action!"
+  end
+
+  def create
+    user = User.new(params.require(:user).permit(:user_attributes_here))
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+end
